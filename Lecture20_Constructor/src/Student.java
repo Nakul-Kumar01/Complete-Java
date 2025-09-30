f51073ad42a2c60b130bcce94dbee363d559e620
Nakul Parmar <parmarnakul277@gmail.com>
2025-09-30 06:04:26 +0530

    Lecture20(constructor)

---
+++ Lecture20_Constructor/src/Student.java
@@ -0,0 +1,47 @@
+
+public class Student {
+
+    String name;
+    int rollNumber;
+    private int age;
+
+
+     // if we overload the constructor, then default constructor will not form
+    public Student(){   // we have overloaded the Default constructor
+        this.age = 0;
+    }
+
+    public Student(String name, int rollNumber, int age){   // we have overloaded the Default constructor
+        this.age = age;
+        this.name = name;
+        this.rollNumber = rollNumber;
+    }
+
+
+    public void setAge(int age){
+        if(age<0) System.out.println("Invalid age");
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name == "") System.out.println("Invalid name");
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        if(rollNumber <= 0) System.out.println("Invalid RollNumber");
+        this.rollNumber = rollNumber;
+    }
+}
---
+++ Lecture20_Constructor/src/construc01.java
@@ -0,0 +1,8 @@
+public class construc01 {
+    public static void main(String[] args) {
+        // constructor is a method used to initialise the object
+
+        Student st = new Student(); // Default Constructor
+
+    }
+}
