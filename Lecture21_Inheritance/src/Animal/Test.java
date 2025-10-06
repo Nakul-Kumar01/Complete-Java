05217f5eb4f4dd588aac4ad695e743674d25f05d
Nakul Parmar <parmarnakul277@gmail.com>
2025-10-06 06:59:28 +0530

    Inheritance & method Overriding02

--- Lecture21_Inheritance/src/Animal/Test.java
+++ Lecture21_Inheritance/src/Animal/Test.java
@@ -1,5 +1,7 @@
 package Animal;
 
+import humans.Child;
+
 public class Test {
 
     public static void main(String[] args){
@@ -15,7 +17,29 @@ public class Test {
    1) single
    2) Multiple
    3) heraical
-   4)
-   */
+
+   - Java does not allow multiInheritance bcoz to avoid ambiguity // for this we have interfaces
+
+
+   super keyword:
+   - we can call parent class constructor, parameterised constructor
+   - we can call parent class methods
+
+
+   new
+   - object is created in heap memory
+   - it is created at runTime not at compile time // Dynamic memory allocation
+   - jbb javac krte hn tbb memory nhi milti, java krke run krte hn tbb memory allocate hoti hai
+
+
+   @Override
+   - good practice to write before the method which we are overriding
+   - code will work fine without this annotation also
+   - but if we use this before our method, then it will give error if the method does not override any parent class method
+   - basically it ensures that the method will override any parent class method
+*/
+
+        Child child = new Child(16,"rohan");  // first GrandParent constructor is called
+        System.out.println(child.age);
     }
 }
---
+++ Lecture21_Inheritance/src/humans/Parent.java
@@ -0,0 +1,13 @@
+package humans;
+
+public class Parent extends Grandparent{
+
+    public Parent(int age, String name){
+        super(age,name);
+        System.out.println("Parent constructor called");
+    }
+
+    public void parentMethod(){
+        System.out.println("Parent method called");
+    }
+}
