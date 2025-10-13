90c09091e2b43302b07205186e6fa0d05353388a
Nakul Parmar <parmarnakul277@gmail.com>
2025-10-13 18:24:58 +0530

    abstraction03

--- Lecture23_Abstraction/src/abstraction/Animal.java
+++ Lecture23_Abstraction/src/abstraction/Animal.java
@@ -8,7 +8,7 @@ public abstract class Animal {
   - Abstract class can contain both Abstract method as well as non Abstract method
   - child class which will inherite Abstract class : Class 'Dog' must either be declared abstract or override abstract methods in 'Animal'
   - object of Abstract class is not allowed.
-  - but we can use Abstract class for reference , Animal dog1 = new Dog();  // aisa hi krte hai for building applications WHY???
+  - but we can use Abstract class for reference , Animal dog1 = new Dog();  // aisa hi krte hai for building applications WHY??
   - Clients focus on what an object does, not how it does it // client can access only override methods
   - also Animal is very genric term, how it will 'say Hello' : so we define abstract method in abstract class(Animal), and their implementation in child class(Dog)
   - Abstract classes should not hv public constructors. Constructors of abstract classes can only be called in constructors of their subclasses(making object is not possible). so there is no point in making them public. the protected modifier should be enough
