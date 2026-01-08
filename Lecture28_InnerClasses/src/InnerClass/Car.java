ba0f413d495227d38d4fcca4089a54aa052a179d
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-08 07:25:59 +0530

    InnerClasses02

--- Lecture28_InnerClasses/src/InnerClass/Car.java
+++ Lecture28_InnerClasses/src/InnerClass/Car.java
@@ -12,21 +12,21 @@ public class Car {
     }
 
     // Inner Class
-    class Engine{
-        void start(){
+    public class Engine{
+        public void start(){
             if(!isEngineOn){
                 isEngineOn = true;
-                System.out.println(model + "Engine started");
+                System.out.println(model + " Engine started");
             }
-            else System.out.println(model + "Engine is already on");
+            else System.out.println(model + " Engine is already on");
         }
 
-        void stop(){
+        public void stop(){
             if(isEngineOn){
                 isEngineOn = false;
-                System.out.println(model + "Engine stopped");
+                System.out.println(model + " Engine stopped");
             }
-            else System.out.println(model + "Engine is already off");
+            else System.out.println(model + " Engine is already off");
         }
     }
 }
--- Lecture28_InnerClasses/src/test.java
+++ Lecture28_InnerClasses/src/test.java
@@ -1,3 +1,5 @@
+import InnerClass.Car;
+
 public class test {
     public static void main(String[] args) {
 
@@ -6,9 +8,16 @@ public class test {
            - 4 types - Member Inner Class , Static Nested Class , Local Inner Class , Anonymous Inner Class
 
            # Member Inner Class:
-             - Inner Class which is also the member of the class // it is associated with instance of outer class
+             - Inner Class which is also the member of the class( **therefore can access properties of class** for this reason only we are using this MIC , vrna class Engine me instance of Car pass krna hota bohot mhent hoti) // it is associated with instance of outer class
              - car hai 'safari' , then hmm 'safari' ka engine bhi create krenge // Car ke object se hmm Engine ko bhi access kr paenge
 
+
          */
+
+
+        Car car = new Car("Safari");
+        Car.Engine engine = car.new Engine(); // creating instance of member Inner class using instance of main class
+        engine.start();
+        engine.stop();
     }
 }
