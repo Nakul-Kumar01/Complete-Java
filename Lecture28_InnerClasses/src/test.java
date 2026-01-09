3f25cb9850142fc76550f90aa13de0a7abf3f5a5
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-09 07:20:24 +0530

    InnerClasses04

--- Lecture28_InnerClasses/src/test.java
+++ Lecture28_InnerClasses/src/test.java
@@ -1,4 +1,4 @@
-import InnerClass.Car;
+import InnerClass.*;
 
 public class test {
     public static void main(String[] args) {
@@ -11,7 +11,15 @@ public class test {
              - Inner Class which is also the member of the class( **therefore can access properties of class** for this reason only we are using this MIC , vrna class Engine me instance of Car pass krna hota bohot mhent hoti) // it is associated with instance of outer class
              - car hai 'safari' , then hmm 'safari' ka engine bhi create krenge // Car ke object se hmm Engine ko bhi access kr paenge
 
-           #
+           # Static Nested Class:
+             - independent of outer class, not dependent on their instance
+             - hmme pata hai dono alag chij hai(Computer and USB), bss hmmne group krdiya hai
+             - kitne bhi computer banalo, USB tho ek hi rahegi
+             - helps in memory Management, sari computer se related chij Computer class me hai
+
+           # Anonymous Inner Class:
+             - if you want to create object of Implementation class without creating Implementation class
+             - if you want to create object for one time use
          */
 
 
@@ -19,5 +27,22 @@ public class test {
         Car.Engine engine = car.new Engine(); // creating instance of member Inner class using instance of main class
         engine.start();
         engine.stop();
+
+        // ### Static Nested Class
+        Computer com = new Computer("HP","ABC", "xyz");
+        com.getOs().displayInfo(); // method of member inner class
+        Computer.USB usb = new Computer.USB("Type-C"); // initialising static Inner class
+
+        // ### Anonymous Inner Class
+        ShoppingCart sc = new ShoppingCart(150);
+//        CreditCard cc = new CreditCard();
+//        sc.processPayment(cc); // ek puri CreditCard class file banai then uska object yaha pass kiya
+
+        sc.processPayment(new Payment() {  // hmne yahi Payment Interface ki implementation class ka object bana diya
+            @Override
+            public void pay(double amount) {
+                System.out.println("Paid " + amount + " using Credit Card.");
+            }
+        });
     }
 }
