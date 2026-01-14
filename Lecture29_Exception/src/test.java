9c3400761c250a8ce36ca634ba1f9d58f1e104fd
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-14 21:45:32 +0530

    exception01

---
+++ Lecture29_Exception/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        /*
+        Types of Errors:
+         - Syntax Error
+         - Logical Error
+         - Runtime Error
+
+
+         */
+    }
+}
