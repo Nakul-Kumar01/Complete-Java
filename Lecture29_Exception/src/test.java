9fd711498dad2d3045ee628aecb9b28873a56d66
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-14 22:00:31 +0530

    exception02

--- Lecture29_Exception/src/test.java
+++ Lecture29_Exception/src/test.java
@@ -6,7 +6,22 @@ public class test {
          - Logical Error
          - Runtime Error
 
+        - Program will crash during runtime error i.e divide by 0
+        - Exception : event that disrupts the normal flow of program. it is an object which is thrown at runtime
+        - Exception handling is a way to handle the runtime errors so that normal flow of the application can be maintained
 
          */
+
+        
+    }
+
+    public static void divide(){
+        try{
+            int a=3,b=0;
+            System.out.println(a/b);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
     }
 }
