a8eafef7293bc40a7a55d98b95efb68300b16b60
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-16 17:25:19 +0530

    wrapper02

--- Lecture30_wrapperClass/src/test.java
+++ Lecture30_wrapperClass/src/test.java
@@ -4,14 +4,24 @@ public class test {
 
         /*
         Wrapper Classes
-        - Java is object-oriented, but primitive types are not objects.
+        - Java is object-oriented, but primitive types are not objects. // therefore we need wrapper classes and these classes are used with Collections (ArrayList, HashMap, etc.)
         - i.e. In Boolean class - has a single field whose type is boolean and In addition, this class provides many methods to object of type Boolean
+        - since ye object store krte hn to can store 'null' but not possible in primitive type
          */
 
         int a = 1; // primitive datatypes // 'a' is variable here // stored in stack
 //        a.  // koi property nhi aaegi
-        Integer b = 1;   // here 'b' is object of Integer class // 1 is stored in heap , 'b' is storing refference // here this 1 get converted from primetive to object
+        Integer b = 1;   // here 'b' is object of Integer class // 1 is stored in heap , 'b' is storing refference // here this 1 get converted from primetive to wrapper class's object called AUTOBOXING
+        a = b;  // AUTOUNBOXING
         boolean hasGraphicCard = true;
         Boolean isAdult = true;
+
+
+        Float f = 1.2f;
+
+
+
+
+        Integer x = null; // can store null
     }
 }
