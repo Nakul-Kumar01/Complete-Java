3a684d6a89a489ce97315520dc2cac9c770be4ff
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-16 17:18:05 +0530

    wrapper01

---
+++ Lecture30_wrapperClass/src/test.java
@@ -0,0 +1,17 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        /*
+        Wrapper Classes
+        - Java is object-oriented, but primitive types are not objects.
+        - i.e. In Boolean class - has a single field whose type is boolean and In addition, this class provides many methods to object of type Boolean
+         */
+
+        int a = 1; // primitive datatypes // 'a' is variable here // stored in stack
+//        a.  // koi property nhi aaegi
+        Integer b = 1;   // here 'b' is object of Integer class // 1 is stored in heap , 'b' is storing refference // here this 1 get converted from primetive to object
+        boolean hasGraphicCard = true;
+        Boolean isAdult = true;
+    }
+}
