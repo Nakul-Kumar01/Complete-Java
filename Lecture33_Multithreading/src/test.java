bc8290c2fd4e62af081d79047104edf77a20df83
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-19 17:04:40 +0530

    multithreading01

---
+++ Lecture33_Multithreading/src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+
+        /*
+        ###  MultiThreading  ###
+        - CPU : responsible for executing instructions from program
+        - core : it is an individual processing unit within a cpu. Modern CPUs can hv multiple cores.
+        - quard-core processor : has 4 cores, allowing it to perform 4 tasks simultaneously
+        - program: set of instructions
+        - process: instance of a program that is being executed
+         */
+    }
+}
