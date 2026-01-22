c4c724f73b97aad4621ca5327c02bbd61b56271c
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-01-22 22:16:10 +0530

    multiThreading10

--- Lecture33_Multithreading/src/test03.java
+++ Lecture33_Multithreading/src/test03.java
@@ -8,9 +8,16 @@ public class test03 extends Thread {
     @Override
     public void run() {
 
-        for (int i=0;i<5;i++){
-            System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " count: " + i);
+//        for (int i=0;i<5;i++){
+//            System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " count: " + i);
+//
+//        }
 
+
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -18,17 +25,28 @@ public class test03 extends Thread {
 
 
         /*
-        - Methods we hv learned: start , run , sleep , join , setPriority
+        - Methods we hv learned: start , run , sleep , join , setPriority, Interrupt , yield
 
         - we can set the priority of the thread( by default 5 hoti hai)
+        - setPriority se hmm jvm and OS ko hint de skte hn ki isski priority high rakna // abb high hogi ya nhi hogi depends on the program and no. of cores // multiCores mei fark pata nhi chalega
 
-
+        - Interrupt : jo bhi Thread kr raha hai usse Interrupt kr do
          */
 
 //        test03 t1 = new test03("Nakul");
 //        t1.start();
 
+//          test03 l = new test03("Low Priority Thread");
+//          test03 m = new test03("Medium Priority Thread");
+//          test03 h = new test03("High Priority Thread");
+//
+//          l.setPriority(Thread.MIN_PRIORITY);
+//          m.setPriority(Thread.NORM_PRIORITY);
+//          h.setPriority(Thread.MAX_PRIORITY);
 
+        test03 t1 = new test03("nakul");
+        t1.start();
+        t1.interrupt(); // give sleep Interrupted
 
     }
 }
