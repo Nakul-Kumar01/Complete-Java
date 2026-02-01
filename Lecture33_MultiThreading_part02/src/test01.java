d6f8dcd6d599a34fdec2e3902be4e2104d2d429a
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-02-01 12:16:48 +0530

    synchronisation02

--- Lecture33_MultiThreading_part02/src/test01.java
+++ Lecture33_MultiThreading_part02/src/test01.java
@@ -3,12 +3,15 @@ public class test01 {
 
         /*
         // why this does not give 6000??
-        - bcoz ek hii time pe dono thread ne counter ki same value utha li
-        - ek time pe ek hi thread access kre kisi method ko : use 'synchronized' to that method
+        - bcoz ek hii time pe dono thread ne counter ki same value utha li  // this is called race condition (when 2 or more threads access the same resource at same time)
+        - ek time pe ek hi thread access kre kisi method ko : use 'synchronized' to that method (since this is shared resources called critical section)
         - if you want only some part of code to be synchronized then use synchronized block
              *** synchronized (this) means:  "Only one thread at a time can execute this block for the same object."
-             - if having different object: tbb to koi chinta hi nhi hai : Both threads run at the same time, No synchronization between them
+             - *** if having different object: tbb to koi chinta hi nhi hai : Both threads run at the same time, No synchronization between them : bcoz, Lock is per object, Different objects = different locks
 
+        - There are 2 types of Locks:
+           1) Intrinsic : these locks are buitin into every object in java. when u use synchronized keyword you are using these automatic locks
+           2) Explicit :
          */
         Counter counter = new Counter();
 
