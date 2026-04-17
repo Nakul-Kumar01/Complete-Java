6f2c578ad8bae96689b5abfecffe98918231895c
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-04-17 07:08:23 +0530

    Callable vs runnable

--- Lecture33_MultiThreading_part3/src/ExecutorFramework03/test01.java
+++ Lecture33_MultiThreading_part3/src/ExecutorFramework03/test01.java
@@ -1,13 +1,10 @@
 package ExecutorFramework03;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class test01 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
         ################   Executors Framework
         - abstracting the complexities involved in creating and managing threads
@@ -25,6 +22,25 @@ public class test01 {
 
         - Future<?>  :  agr  kuch return kra hai task ne tho vo store krne ke liye
 
+
+        - callable Interface vs runnable Interface
+| Feature          | Runnable     | Callable       |
+| ---------------- | ------------ | ---------------------- |
+| Return value     | ❌ No         | ✅ Yes (Future<?>    |
+| Method           | `run()`      | `call()`               |
+| Throws exception | ❌ No         | ✅ Yes                |
+| Package          | `java.lang`  | `java.util.concurrent` |
+| Usage            | Simple tasks | Tasks needing result   |
+
+
+           - use Runnable when u not want to return any thing
+           - use Callable when u want to return any thing
+
+           -*** executor can accept both runnable as well as callable interface Implementation
+
+
+           - task in executor.submit() is not executed my main thread , it is done by threads in thread Pool
+           - but when we use ans.get() then main thread will wait for completion
          */
 
 
@@ -62,7 +78,7 @@ public class test01 {
         for(int i=1;i<10;i++){
             int finalI = i;
 
-            Future<?> submit = executor.submit(() -> {
+            Future<?> submit = executor.submit(() -> {   // abb jo ye task hai issko main thread execute nhi krega
                 long result = factorial(finalI);
                 System.out.println(result);
             });
@@ -83,6 +99,10 @@ public class test01 {
 
         System.out.println("total time: " + (System.currentTimeMillis()-startTime));
 
+
+
+
+
         // working of future  // to get datatype and variable automatically use alt+Enter
         ExecutorService executor1 = Executors.newSingleThreadExecutor();
         Future<Integer> res = executor1.submit(() -> 42);
@@ -93,6 +113,42 @@ public class test01 {
         catch (Exception e){
 
         }
+
+
+
+        // callable vs runnable
+        ExecutorService executor2 = Executors.newSingleThreadExecutor();
+//        Future<String> ans = executor2.submit(() -> "hello"); // working fine // here submit is accepting callable task(since lambda fn is returning something)
+//        Runnable runnable = () -> "hello";   // runnable task does not return something, but callable task can return
+        Callable callable = () -> "hello";
+        Future<String> ans = executor2.submit(callable);
+
+        Future<?> ans2 = executor2.submit(()-> System.out.println("nakul"),"is successfull"); // after successfull completion of runnable method, it will return this result
+        executor2.shutdown();
+
+
+
+        // understanding the flow :
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<Integer> submit = executorService.submit(() -> 1 + 2);  // this task will be executed by 2 threads in Pool, main thread will not wait
+        Integer i =0;
+        try{
+             i = submit.get();    // but here main thread needs to wait for completion of task given to 2 threads
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("sum is " + i);
+        executorService.shutdown();
+        System.out.println(executorService.isShutdown());  // true
+        System.out.println(executorService.isTerminated());// why returning false ?? -> abhi shutdown kiya, then turnt print krva diya , kuch time tho dete!! (add Thread.sleep(1) )
+        // Returns true if all tasks have completed following shut down
+         // Note that isTerminated is never true unless either shutdown or shutdownNow was called first.
+
+        Thread.sleep(1);
+        System.out.println(executorService.isTerminated()); // now give true
 
     }
 
